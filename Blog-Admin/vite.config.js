ec0222af85cd696e9645b54ca31ee7197930cbf2
IK32K3 <hoangviet232003@gmail.com>
2025-03-30 22:08:47 +0700

    Update

--- Blog-Admin/vite.config.js
+++ Blog-Admin/vite.config.js
@@ -27,7 +27,6 @@ export default defineConfig({
         userList: resolve(__dirname, 'src/pages/users/index.html'),
         postList: resolve(__dirname, 'src/pages/posts/index.html'),
         categoryList: resolve(__dirname, 'src/pages/categories/index.html'),
-        commentList: resolve(__dirname, 'src/pages/comments/index.html'),
         languageList: resolve(__dirname, 'src/pages/languages/index.html'),
         settings: resolve(__dirname, 'src/pages/settings/index.html'),
       }
